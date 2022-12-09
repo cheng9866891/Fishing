c14b208b0ff320c5aff9c4825dea0454da8f9752
cheng <542423571@qq.com>
2022-12-09 16:06:31 +0800

    test-1

--- app/src/main/java/a/cheng/com/myapplication/MainActivity.java
+++ app/src/main/java/a/cheng/com/myapplication/MainActivity.java
@@ -13,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
         Log.i("MainActivity","增加日志打印");
         Log.i("MainActivity","修改日志打印");
         Log.i("MainActivity","修改日志打印2");
+        Log.i("MainActivity","修改日志打印3");
+        Log.i("MainActivity","修改日志打印4");
+        Log.i("MainActivity","修改日志打印5");
+        Log.i("MainActivity","修改日志打印6");
         Log.i("MainActiivty","--fixup");
     }
 }
